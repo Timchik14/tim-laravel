633e8cf2ecf69e35a075a802f1b187bb4f156627
Timchik14 <temurtheg@rambler.ru>
2022-03-10 21:23:02 +0500

    autorization added

---
+++ resources/views/layouts/errors.blade.php
@@ -0,0 +1,10 @@
+@if($errors->count())
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const modal = new bootstrap.Modal(document.querySelector('#modal'));
+            modal.show();
+        })
+    </script>
+@endif
+
+@include('layouts.modal')
