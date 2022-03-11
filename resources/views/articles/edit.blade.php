96b7e118cf1ed22536f7811510211f30bcc6dc4a
Timchik14 <temurtheg@rambler.ru>
2022-03-11 15:54:45 +0500

    small changes

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('title', 'Создать статью')
+@section('title', 'Изменить статью')
 @section('content')
     <div class="col-md-8 blog-main">
         <h3 class="pb-3 mb-4 font-italic border-bottom">
