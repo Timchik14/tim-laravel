d16b51269ac41cceeb023516b39c87f3213318d1
Timchik14 <temurtheg@rambler.ru>
2022-04-06 14:32:40 +0500

    small changes

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -43,8 +43,10 @@
     <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
             <a class="p-2 text-muted" href="{{ route('main') }}">Главная - все статьи</a>
-            <a class="p-2 text-muted" href="{{ route('articles.index') }}">Мои статьи</a>
-            <a class="p-2 text-muted" href="{{ route('articles.create') }}">Создать статью</a>
+            @auth()
+                <a class="p-2 text-muted" href="{{ route('articles.index') }}">Мои статьи</a>
+                <a class="p-2 text-muted" href="{{ route('articles.create') }}">Создать статью</a>
+            @endauth
             <a class="p-2 text-muted" href="{{ route('about') }}">О нас</a>
             <a class="p-2 text-muted" href="{{ route('contacts') }}">Контакты</a>
             @admin
